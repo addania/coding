f731fca7de565d9d0b12edb86d475bf50dfa81dd
addania <mirka.schw@gmail.com>
2019-10-20 14:58:33 +0200

    fixing the link to contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 export default () => 
 
 <div style={{color: "purple", fontSize:"72px"}}>
-  <Link to="/contact/">Contact</Link>
+  <Link to="/coding/contact/">Contact</Link>
   <h1>Hello Gats!!</h1>
   <p>What a world</p>
   <img src="https://source.unsplash.com/random/400x200" alt=""/>
