663e6a3fe96a1265a64ed572d09bed0126bc88b7
addania <mirka.schw@gmail.com>
2019-11-11 23:24:48 +0100

    Updates

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.__precacheManifest = [
     "url": "styles.f324fa64a3040c6ae310.css"
   },
   {
-    "url": "styles-ea5dc67354b5aaad05d2.js"
+    "url": "styles-a3e7665f750d126964f4.js"
   },
   {
     "url": "commons-59a9698dfe46d2d95d64.js"
@@ -46,11 +46,11 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "feba02b14e2d5bc65bfb24e625681cba"
+    "revision": "157effe461ed0655998ddf82b94e8c68"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "f631c0a273f0d4d939bd68bf95833b53"
+    "revision": "67cc36737d08f168002ddf11cc8f2db1"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
