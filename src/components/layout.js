7bd8b0ced6eed33108099955c99af54611b619df
addania <mirka.schw@gmail.com>
2020-03-03 07:54:03 +0100

    adding Impressions section to the nav bar and as a page, in order to write down some impressions and thoughts from my journey as a Front-End developer- will  be fun (because there will is  cake)

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link, useStaticQuery, graphql } from "gatsby";
+import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -8,37 +8,49 @@ const ListLink = props => (
 )
 
 export default ({ children }) => {
-const data = useStaticQuery (
-  graphql`
-  query {
-    site {
-      siteMetadata{
-        title
-        author
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            author
+          }
+        }
       }
-    }
-  }
-  `
-)
-return (
-  <div style={{ margin: `3rem auto`, maxWidth: 800, padding: `0 1rem` }}>
-    <header style={{ marginBottom: `1.5rem`, paddingTop:"20px" }}>
-      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline`, fontSize:"1rem" }}>{data.site.siteMetadata.author}</h3>
-      </Link>
-      <ul style={{ listStyle: `none`, float: `right`}}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
+    `
+  )
+  return (
+    <div style={{ margin: `3rem auto`, maxWidth: 800, padding: `0 1rem` }}>
+      <header style={{ marginBottom: `1.5rem`, paddingTop: "20px" }}>
+        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+          <h3 style={{ display: `inline`, fontSize: "1rem" }}>
+            {data.site.siteMetadata.author}
+          </h3>
+        </Link>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/impressions/">Impressions</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+        <hr />
+      </header>
+      {children}
+
       <hr />
-    </header>
-    {children}
-    
-    <hr />
-    <footer style={{ paddingBottom: "20px" }}>
-        <p style={{  textAlign: "center", fontSize: "14px", marginBottom:"0", color:"grey"}}>© 2019 Design and Development by {data.site.siteMetadata.author}</p>
-    </footer>
-  </div>
-)
+      <footer style={{ paddingBottom: "20px" }}>
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: "14px",
+            marginBottom: "0",
+            color: "grey",
+          }}
+        >
+          © 2019 Design and Development by {data.site.siteMetadata.author}
+        </p>
+      </footer>
+    </div>
+  )
 }
---
+++ src/pages/impressions.js
@@ -0,0 +1,95 @@
+import React from "react"
+import Layout from "../components/layout.js"
+import Header from "../components/header.js"
+import { graphql } from "gatsby"
+import SEO from "../components/seo.js"
+import { Carousel } from "../components/carousel.js"
+
+export default ({ data }) => (
+  <Layout>
+    <h2 style={{ textAlign: "center" }}>Dev Impressions</h2>
+    <p style={{ textAlign: "center" }}>
+      infiltrating the dev universe in order to <s>conquer it</s> learn a lot ;)
+    </p>
+    <p style={{ textAlign: "justify", textJustify: "inter-word" }}>
+      <img
+        src="https://i.imgur.com/zcYbg1V.jpg"
+        alt="Photo by Gereth Davies from Pexels"
+      />
+      <strong>Dev day 0: 02.03.2020</strong>
+      <br />
+      <blockquote>
+        The day to be remembered... My very very <code>1st </code>
+        day as a Front-End Developer... The day <code>0</code>, since obviously
+        all acceptably decent indices shall start with zero.
+      </blockquote>
+      <br />A day a bit chaotic as I was settling in my new corner of the
+      office.
+      <br />
+      <br />
+      But a day with a <code>MohnKuchen</code> which propheted an excellent day.
+      After all, what can go wrong when you have a prospect of a delicious cake
+      and warm cup of tea from our CTO (Chief Tea Officer) to combat literally
+      ANY challenge in life? They say <code>piece of cake</code> for a reason,
+      don't they? :)
+      <br />
+      <br />A day when I <i>actually</i> talked to <i>actual</i> developers. And
+      they
+      <i> actually</i> talked BACK :) Thank you <code>MohnKuchen</code> ;)
+      <br />
+      <br />A day with <code>muscle tomcat</code>. Yes, yes, dear Austrians: ein
+      Tag mit Muskelkater :) from previous day(s) of intense training and ...
+      let's admit it... carrying that <code>MohnKuchen</code> all the way to the
+      office.
+      <br />
+      <br />A day deeply satisfying as I finally achieved my long desired goal.
+      The hard work for past months has finally paid off. Dream turning
+      inevitably into reality right in front of my eyes. It seems a bit surreal,
+      it seems a bit... well plain even, earthy maybe. But <code>MINE</code>.
+      <br />
+      <br />A day with a small <code>victory</code> to setup my first
+      professional dev environment.
+      <br />
+      <br />A day which ended up with an intriguing <code>puzzle</code> to look
+      forward to solving the next day :)
+      <br />
+      <br />A day with an after-work <code>hangout</code> which turned out to be
+      more pleasant than expected.
+      <br />
+      <blockquote>Total score: The day freaking ROCKED :)</blockquote>
+      <br />
+      And yes, the <code>cake</code> was the most important part if it :)
+      <br />
+      <img
+        src="https://i.imgur.com/aRwy8mP.jpg"
+        alt="Photo by Antonio Quagliata from Pexels"
+      />
+      <strong>Dev day 1: 03.03.2020</strong>
+      <br />
+      <blockquote>
+        First things first... yes, leftovers from the <code>cake </code>
+        are still in the fridge... will be another great day :)
+      </blockquote>
+      <br />
+      And now back to the <code>puzzle</code> from yesterday: how to hack the
+      unhackable login to the app on my LOCAL? My wildly lucky guess{" "}
+      <code>admin admin</code> did not work. But I got an insider tip that the
+      second half of the riddle I have solved already, muhahahaha. So now only
+      to get the first one right.
+      <br />
+      <br />
+      Tadaaa! Mission accomplished! Addania +1 Hacker Skills. It is only my
+      second day and I am totally pro already :)) ...*cough cough*
+    </p>
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
