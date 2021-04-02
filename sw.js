c79068ed1ba61f33cb61d7ec275764f26bf34eff
addania <mirka.schw@gmail.com>
2021-04-02 15:21:08 +0200

    Updates

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.__precacheManifest = [
     "url": "styles.0b9930982994ab769135.css"
   },
   {
-    "url": "styles-7c9560633b2e57af3b2e.js"
+    "url": "styles-39d4a022c0d70b5ab7ca.js"
   },
   {
     "url": "commons-6561a93d1bb0938673fd.js"
@@ -46,11 +46,11 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "dfe93a41024a666622cd1c295cf3ca76"
+    "revision": "365463d526a78d91de8d08ffc8ac53f3"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "6d89d6052a0d5b25e40ba3c7a8361eb1"
+    "revision": "0782c6880761d1e9bae176dc22b26647"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
