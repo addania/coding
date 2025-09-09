a5b6fd3178fa7a22ec26f852755ed0297e223dd1
addania <mirka.schw@gmail.com>
2025-09-09 10:47:38 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-424c21382b46a4c734ff.js"
+    "url": "webpack-runtime-8061b0bd1fcf644b84c5.js"
   },
   {
     "url": "styles.89c452e312c865d83043.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a3c2e86d2a895568c54d920fc451bf77"
+    "revision": "dc58e06ea4e07099cedd2cfbc1ed614e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-2f9004dcb1f826313e67.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "2b440af1a05ff59f0be8bca16b4d4334"
+    "revision": "620e286eaf7f283844026bc79057f748"
   },
   {
     "url": "polyfill-0c10fa13d8ab6f477916.js"
